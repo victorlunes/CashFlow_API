d36ee1aeeda657ab2b46e68efa0a50860509ea58
Victor Lunes <victorllunes@gmail.com>
2025-11-24 23:35:26 -0300

    feat: improving unit tests

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,5 +1,8 @@
 using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
+using CashFlow.Exception;
+using CommonTestUtilities.Request;
 
 namespace Validators.Tests.Expenses.Register;
 
@@ -12,19 +15,86 @@ public class RegisterExpenseValidatorTests
         //Arrange
         var validator = new RegisterExpenseValidator();
 
-        var request = new RequestRegisterExpenseJson
-        {
-            Amount = 100,
-            Date = DateTime.Now.AddDays(-1),
-            Description = "descrição",
-            PaymentType = CashFlow.Communication.Enums.PaymentType.DebitCard,
-            Title = "Apple"
-        };
-
+        var request = RequestRegisterExpenseJsonBuilder.Build();
         //Act
         var result = validator.Validate(request);
 
         //Assert
         Assert.True(result.IsValid);
+    }
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData("     ")]
+    [InlineData(null)]
+    public void Error_Title_Empty(string title)
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = title;
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, e => e.ErrorMessage == ResourceErrorMessage.TITLE_REQUIRED);
+    }
+    
+    [Fact]
+    public void Error_Date_Future()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, e => e.ErrorMessage == ResourceErrorMessage.EXPENSES_CANNOT_FOR_THE_FUTURE);
+    }
+    
+    [Fact]
+    public void Error_PaymentType_Invalid()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.PaymentType = (PaymentType)8;
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, e => e.ErrorMessage == ResourceErrorMessage.PAYMENT_TYPE_INVALID);
+    }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-7)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Amount = amount;
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, e => e.ErrorMessage == ResourceErrorMessage.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
     }
 }
