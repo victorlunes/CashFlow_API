adf0bbc2410f01cc06f7ffd183216c298f853e50
Victor Lunes <victorllunes@gmail.com>
2025-11-30 19:26:46 -0300

    feat: An instance was created to connect to the database.

---
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -0,0 +1,18 @@
+using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Entities;
+
+public class Expense
+{
+    public long Id { get; set; }
+    
+    public string Title { get; set; } = String.Empty;
+    
+    public string? Description { get; set; }
+    
+    public DateTime Date { get; set; }
+    
+    public decimal Amount { get; set; }
+    
+    public PaymentType PaymentType { get; set; }
+}
