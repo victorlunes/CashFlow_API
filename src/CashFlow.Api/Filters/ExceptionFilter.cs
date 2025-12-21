6aa1555058f9627e64ec97b910609bef7bd38bb5
Victor Lunes <victorllunes@gmail.com>
2025-12-20 21:06:40 -0300

    fix: improving exception routine

--- src/CashFlow.Api/Filters/ExceptionFilter.cs
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -22,26 +22,12 @@ public class ExceptionFilter : IExceptionFilter
 
     public void HandleProjectException(ExceptionContext context)
     {
-        if (context.Exception is ErrorOnValidationException errorOnValidationException)
-        {
-            var errorResponse = new ResponseErrorJson(errorOnValidationException.Errors);
-            
-            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            context.Result = new BadRequestObjectResult(errorResponse);
-        }
-        else if (context.Exception is NotFoundException notFoundException)
-        {
-            var errorResponse = new ResponseErrorJson(notFoundException.Message);
-            
-            context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-            context.Result = new NotFoundObjectResult(errorResponse);
-        }
-        else
-        {
-            var errorResponse = new ResponseErrorJson(context.Exception.Message);
-            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            context.Result = new BadRequestObjectResult(errorResponse);
-        }
+        var cashFlowException = (CashFlowException)context.Exception;
+        var errorResponse = new ResponseErrorJson(cashFlowException.GetErrors());
+        
+
+        context.HttpContext.Response.StatusCode = cashFlowException.StatusCode;
+        context.Result = new ObjectResult(errorResponse);
     }
     
     public void ThrowUnkowError(ExceptionContext context)
